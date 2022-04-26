e530838d8269b26d35a8c88e020b61e24982c566
Kagin007 <adamschulte148@hotmail.com>
2022-04-26 15:14:15 -0400

    finished daylist item conditional

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ import classNames from "classnames";
 
 export default function Button(props) {
 
-   let buttonClass = classNames('button ', {'button--confirm': props.confirm}, {'button--danger': props.danger})
+   let buttonClass = classNames('button ', {'button--confirm': props.confirm}, {'button--danger': props.danger}, {'button--dodo': props.dodo})
 
    return <>
       <button disabled={props.disabled} className={buttonClass} onClick={props.onClick}> {props.children}</button>
